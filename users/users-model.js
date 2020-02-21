ccfc530dc1c4b43356b4ec57cf2fe83fa82ee3e3
Chris Vasquez <chrvasq@outlook.com>
2020-02-20 17:46:33 -0800

    Add required endpoints

---
+++ users/users-model.js
@@ -0,0 +1,29 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
+
+function find() {
+  return db("users").select("id", "username", "department");
+}
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username", "department")
+    .where({ id })
+    .first();
+}
