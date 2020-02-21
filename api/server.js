e799749f34bfe61fbb823a7d6b2ffb96ff430fb6
Chris Vasquez <chrvasq@outlook.com>
2020-02-20 16:55:58 -0800

    Update dependencies and setup server

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const configureMiddleware = require("./configure-middleware");
+
+const server = express();
+
+configureMiddleware(server);
+
+module.exports = server;
